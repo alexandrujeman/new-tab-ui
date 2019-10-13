1a506079f504fe13faf93938ff065b2075d614fd
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-10-13 14:43:42 +0300

    Add reset option

--- js/options.js
+++ js/options.js
@@ -18,21 +18,41 @@ function save_options() {
   );
 }
 
+function reset_options() {
+  var colors = "#FFFFFF";
+  var name = null;
+  chrome.storage.sync.set(
+    {
+      clockColor: colors,
+      firstName: name
+    },
+    function() {
+      // Update status to let user know options were reset.
+      var status = document.getElementById("status");
+      status.textContent = "Options Reset.";
+      setTimeout(function() {
+        status.textContent = "";
+        restore_options();
+      }, 750);
+    }
+  );
+}
+
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get(
     {
-      clockColor: "#FF0000",
+      clockColor: "#FFFFFF",
       firstName: "",
     },
     function(items) {
       document.getElementById("colors").value = items.clockColor;
       document.getElementById("name").value = items.firstName;
-      console.log(items.firstName)
     }
   );
 }
 document.addEventListener("DOMContentLoaded", restore_options);
 document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("reset").addEventListener("click", reset_options);
